7f92197b28bd7ffe2a1f0ed9622584558c19a04a
yangglemail@163.com <yangglemail@163.com>
2020-06-13 19:04:05 +0800

    新建工程

---
+++ src/main/java/com/ygl/base/singleton/Singleton.java
@@ -0,0 +1,90 @@
+package com.ygl.base.singleton;
+
+/**
+ * @USER: rookie_ygl
+ * @DATE: 2020/6/13
+ * @TIME: 12:33
+ * @DESC: open stack
+ **/
+
+/**
+ * 饿汉式 基本不会使用
+ */
+class HuangerSingleton {
+    private static HuangerSingleton huangerSingleton = new HuangerSingleton();
+
+    //构造方法私有化 只能通过getInstance获取
+    private HuangerSingleton() {
+    }
+
+    public static HuangerSingleton getInstance() {
+        return huangerSingleton;
+    }
+
+    //默认权限修饰符是default
+    void getFunction() {
+
+    }
+}
+
+/**
+ * 线程安全，调用效率不高，但是能延时加载
+ */
+class LazySingleton {
+    private static LazySingleton lazySingleton;
+
+    private LazySingleton() {
+
+    }
+
+    public static synchronized LazySingleton getInstance() {
+        if (lazySingleton == null) {
+
+            lazySingleton = new LazySingleton();
+        }
+        return lazySingleton;
+    }
+
+}
+
+//双重锁判断机制（由于JVM底层模型原因，偶尔会出问题，不建议使用）
+class DoubleCheckSingleton {
+    //volatile 保证有序
+    private volatile static DoubleCheckSingleton doubleCheckSingleton;
+
+    private DoubleCheckSingleton() {
+    }
+
+    public static DoubleCheckSingleton getInstance() {
+        if (doubleCheckSingleton == null) {
+            synchronized (DoubleCheckSingleton.class) {
+                if (doubleCheckSingleton == null) {
+                    doubleCheckSingleton = new DoubleCheckSingleton();
+                }
+
+            }
+        }
+        return doubleCheckSingleton;
+    }
+}
+
+/**
+ * 静态内部类实现模式（线程安全，调用效率高，可以延时加载）
+ */
+class StaticSingleton {
+    private static class StaticSingletonInstance {
+        private static final StaticSingleton STATIC_SINGLETON = new StaticSingleton();
+    }
+
+    private StaticSingleton() {
+    }
+
+    public static StaticSingleton getInstance() {
+        return StaticSingletonInstance.STATIC_SINGLETON;
+    }
+
+}
+
+public class Singleton {
+    HuangerSingleton huangerSingleton = HuangerSingleton.getInstance();
+}
--- src/test/java/com/ygl/YglApplicationTests.java
+++ src/test/java/com/ygl/YglApplicationTests.java
@@ -3,11 +3,28 @@ package com.ygl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URL;
+
 @SpringBootTest
 class YglApplicationTests {
 
     @Test
     void contextLoads() {
+
+        URL resource = this.getClass().getResource("");
+        ClassLoader classLoader = this.getClass().getClassLoader();
+
+        URL resource1 = classLoader.getResource("");
+
+
+        System.out.println(resource1.getPath());
+        System.out.println(resource.getPath());
+
+        String path = this.getClass().getClassLoader().getResource("application.yml").getPath();
+        System.out.println(path);
+
+        URL resource2 = Thread.currentThread().getContextClassLoader().getResource("");
+        System.out.println(resource2.getPath());
     }
 
 }
