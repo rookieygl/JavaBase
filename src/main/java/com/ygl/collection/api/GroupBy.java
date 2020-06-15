35a706c9f0425c9bfc2428395598e3225cf7a025
ygl <yangglemail@163.com>
2020-06-15 17:07:37 +0800

    java 反射

--- src/main/java/com/ygl/collection/api/GroupBy.java
+++ src/main/java/com/ygl/collection/api/GroupBy.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @Author: open stack
@@ -16,19 +17,19 @@ public class GroupBy {
 		List<Map<String, String>> list = new ArrayList<>();
 		Map<String, String> znameMap = new HashMap<>(100);
 		Map<String, String> wnameMap = new HashMap<>(100);
-		//
-		//znameMap.put("surName","张");
-		//znameMap.put("name","四");
-		//znameMap.put("surName","张三");
-		//znameMap.put("name","四");
-		//
-		//wnameMap.put("surName","王");
-		//wnameMap.put("name","二麻子");
-		//list.add(znameMap);
-		//list.add(wnameMap);
-		//Map<String, List<Map<String, String>>> filter_type = list.stream().collect(Collectors.groupingBy((v) -> v.keySet().toString()));
-		//filter_type.forEach((surName,fullName)->{
-		//	System.out.println(surName+fullName);
-		//});
+
+		znameMap.put("surName","张");
+		znameMap.put("name","四");
+		znameMap.put("surName","张三");
+		znameMap.put("name","四");
+
+		wnameMap.put("surName","王");
+		wnameMap.put("name","二麻子");
+		list.add(znameMap);
+		list.add(wnameMap);
+		Map<String, List<Map<String, String>>> filter_type = list.stream().collect(Collectors.groupingBy((v) -> v.keySet().toString()));
+		filter_type.forEach((surName,fullName)->{
+			System.out.println(surName+fullName);
+		});
 	}
 }
