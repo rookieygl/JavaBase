865d84bfd6d75512d5d9f251ed4df6571c94fd3b
ygl <yangglemail@163.com>
2020-07-08 17:49:42 +0800

    hadoop起步

---
+++ src/test/java/com/ygl/operation/BinaryOperation.java
@@ -0,0 +1,18 @@
+package com.ygl.operation;
+
+import org.junit.Test;
+
+/**
+ * @Project: ydzy-report
+ * @Author: ygl
+ * @Date: 2020/7/8 16:56
+ * @Desc:
+ */
+public class BinaryOperation {
+
+	@Test
+	public void decimalOperation() {
+		System.out.println("3 * 0.1 = " + 3 * 0.1);
+		System.out.println("0.3 * 1 = " + 0.3 * 1);
+	}
+}
