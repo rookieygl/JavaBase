b2ca484ec6fc90c361128f69363dc49f3483a0e4
yangglemail@163.com <yangglemail@163.com>
2020-07-21 23:56:42 +0800

    dp

--- src/main/java/com/ygl/algorithm/GreatestCommonDivisor/GreatestCommonDivisor.java
+++ src/main/java/com/ygl/algorithm/GreatestCommonDivisor/GreatestCommonDivisor.java
@@ -10,7 +10,9 @@ package com.ygl.algorithm.GreatestCommonDivisor;
  * <p>
  * 结合欧几里得算法（辗转相处法）和九章算术
  * <p>
- * 欧几里得：两个正整数a和b（a>b） 他们的最大公约数等于a除以b的余数c和b之间的最大公约数
+ * 欧几里得：两个正整数a和b（a>b），他们的最大公约数等于a除以b的余数c和b之间的最大公约数
+ *
+ * 九章算术：两个正整数a和b（a>b），他们的最大公约数等于a-b的差值c和较小数b的最大公约数。
  **/
 public class GreatestCommonDivisor {
     public static void main(String[] args) {
@@ -37,8 +39,8 @@ public class GreatestCommonDivisor {
     /**
      * 九章算术获取最大公约数
      *
-     * @param a
-     * @param b
+     * @param a 参数
+     * @param b 参数
      * @return
      */
     public static int getGCDByJiuZang(int a, int b) {
@@ -48,6 +50,44 @@ public class GreatestCommonDivisor {
         int big = a > b ? a : b;
         int small = a < b ? a : b;
         return getGCDByJiuZang(big - small, small);
+    }
+
+    /**
+     * 欧几里得和九章算术结合
+     *
+     * 偶数的最低位一定是0
+     * 奇数的最低位一定是1
+     * 所以如果要判断这个数是奇数还是偶数，只要用这个数按位与1就可以了。
+     * 如果结果为1，那么就是奇数，如果结果为0，那么结果就是偶数。
+     *
+     * 该运算效率比与2取模效率高跟多。
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int gcd(int a,int b){
+        //a==b 互为最大公约数
+        if (a==b) {
+            return a;
+        }
+
+        //a，b均为偶数时，gcd=2xgcd(a/2,b/2)=gcd(a>>1,b>>1)<<1
+        if ((a&1)==0 && (b&1)==0){
+            return gcd(a>>1,b>>1)<<1;
+
+
+        //如果a为偶数，b为奇数时，gcd=gcd(a/2,b)=gcd(a>>1,b)
+        }else if ((a&1)==0 && (b&1)!=0){
+            return gcd(a>>1,b);
+        }else if ((a&1)!=0&& (b&1)==0){
+            return gcd(a,b>>1);
+
+        //a,b均为奇数时，利用更相减损术运算一次，此时a-b，必然是偶数，继续进行位移运算
+        }else {
+            int big = a>b?a:b;
+            int small = a<b?a:b;
+            return gcd(big-small,small);
+        }
     }
 
 }
---
+++ src/main/java/com/ygl/algorithm/IsPowerOf2/ISPowerOf2.java
@@ -0,0 +1,35 @@
+package com.ygl.algorithm.IsPowerOf2;
+
+/**
+ * @Project open sources
+ * @Author: rookie_ygl
+ * @DATE: 2020/7/20 23:05
+ * @DESC: rookie stronger
+ *
+ * 判断一个数是否为2的整数次幂
+ **/
+public class ISPowerOf2 {
+    public static void main(String[] args) {
+        System.out.println(isPowerOf2(256));
+        System.out.println(0&1);
+        System.out.println(0|1);
+        System.out.println(1|0);
+    }
+
+    /**
+     *一个整数num是2的整数次幂，转成二进制时，只有最高位为1，其余位为0
+     * 而该整数减1，转成二进制，所有位位1
+     *
+     * 那么(num & num - 1)==0
+     *
+     * 比如
+     *    二进制 减1二进制  (num & num - 1)
+     * 8  1000   111       0
+     * 0&1 = 0 0&0 = 1
+     * @param num
+     * @return
+     */
+    public static boolean isPowerOf2(int num) {
+        return (num & num - 1) == 0;
+    }
+}
